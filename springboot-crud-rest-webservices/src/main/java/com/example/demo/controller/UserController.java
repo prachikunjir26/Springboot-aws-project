c760608ad428b1181a659d11083379cd4be56742
Prachi Kunjir <prachikunjir11@gmail.com>
2023-05-27 19:19:03 -0400

    First commit

---
+++ springboot-crud-rest-webservices/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	//get all users
+	@GetMapping
+	public List<User> getAllUsers(){
+		
+		return this.userRepository.findAll();
+	}
+	//get user by id
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable (value = "id") long userId) {
+		
+		return this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+	}
+	//create user
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		return this.userRepository.save(user);
+	}
+	//update user
+	@PutMapping("/{id}")
+	public User updateUser(@RequestBody User user, @PathVariable ("id") long userId) {
+		User existingObject =  this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+		existingObject.setFirstName(user.getFirstName());
+		existingObject.setLastName(user.getLastName());
+		existingObject.setEmail(user.getEmail());
+		
+		return this.userRepository.save(existingObject);
+	}
+	//delete user by id
+	@DeleteMapping("/{id}")
+	public ResponseEntity<User> deleteUser(@PathVariable ("id") long userId) {
+		User existingObject =  this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+		
+		this.userRepository.delete(existingObject);
+		return ResponseEntity.ok().build(); //will return 200 response
+	}
+}
